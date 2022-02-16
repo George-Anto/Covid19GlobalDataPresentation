e7d7fc3f43125f10512534a3300ccd72b7df813c
George-Anto <george3osfp@gmail.com>
2022-02-16 21:28:11 +0200

    Some comments added

--- src/main/java/Country.java
+++ src/main/java/Country.java
@@ -1,6 +1,12 @@
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+//The objects that are created of this class contain the data that the user requested
+//These objects contain the aggregated data for a country or monthly, as per user request
+//When the data are aggregated, the object is created through the gson library and the fields of
+//the object are set from the json that the api http call returns
+//If the type is monthly, the data are drawn from the csv file located in resources folder of the project
+//so, in that case we create the object manually with the constructor and set the corresponding fields ourselves
 public class Country {
 
     @Expose
@@ -11,14 +17,15 @@ public class Country {
     @SerializedName("ts")
     @Expose
     private final long queryTimestamp;
-    private String typeOfData; // If data by country (total): "aggregated", if data by month: "monthly"
+    //If data by country (total): "aggregated", if data by month: "monthly"
+    private String typeOfData;
 
     public Country(Data data, String queryDate, long queryTimestamp) {
         this.data = data;
         this.queryDate = queryDate;
         this.queryTimestamp = queryTimestamp;
     }
-
+    //Getters
     public String getQueryDate() { return queryDate; }
     public long getQueryTimestamp() { return queryTimestamp; }
     public String getTypeOfData() { return typeOfData; }
@@ -30,17 +37,24 @@ public class Country {
     public String getMonth() { return data.getMonth(); }
     public String getYear() { return data.getYear(); }
     public double getAverageCumulativeNumberOfCasesFor2Weeks() { return data.getAverageCumulativeNumberOfCasesFor2Weeks(); }
-
+    //Setter
     public void setTypeOfData(String typeOfData) { this.typeOfData = typeOfData; }
 
+    //This method prints the most relevant information of the country object that is calling it
     public void printCountryData() {
+        //If the type is aggregated, the following fields will be presented to the user
         if (this.getTypeOfData().equals( "aggregated")) {
             System.out.println("Getting aggregated results for country: " + this.getCountryName() + "\n" +
                     "Total covid cases confirmed: " + this.getConfirmed() + "\n" +
                     "Total deaths due to the pandemic: " + this.getDeaths() + "\n");
 
+            //The thread that is in charge of mining the block that will
+            //store the country data and then save it to the database
+            //We call the one and only instance of the Miner class and pass it the country object
+            //that is shown to the user, so it will be stored in the block and then the database
             Miner.getInstance().writeCountryToBlock(this);
         }
+        //If the type is monthly, then the information shown are the following
         else if(this.getTypeOfData().equals( "monthly")) {
             System.out.println("Getting monthly results for country: " + this.getCountryName() + "\n" +
                     "Month displayed: " + this.getMonth() + "/" + this.getYear() + "\n" +
@@ -49,12 +63,18 @@ public class Country {
                     String.format("%.2f", this.getAverageCumulativeNumberOfCasesFor2Weeks()) + "\n" +
                     "Deaths this month due to the pandemic: " + this.getDeaths() + "\n");
 
+            //We do the same here, we did not put the call of this method outside the if clause
+            //because we will not call it if the type is neither aggregated nor monthly
             Miner.getInstance().writeCountryToBlock(this);
 
         }
+        //Else if the type is neither type of the above, the object does not contain valid data
         else System.out.println("We could not find any data for your input.\n");
     }
 
+    //This method returns a string that contains all the data of the current object
+    //It is relevant for the mining of the block that the thread is doing before the
+    //mined block that contains the data is stored to the database
     @Override
     public String toString() {
         return this.data.toString();
--- src/main/java/DataSource.java
+++ src/main/java/DataSource.java
@@ -16,26 +16,36 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+//The instance of this class that is created in the Menu class is responsible for getting the data of the api
+//or the csv and create and return a Country object that contains the data the user requested
 public class DataSource {
 
     private final String myFile;
     private BufferedReader reader;
 
+    //the constructor is private
     private DataSource() {
         myFile = "resources/data.csv";
         reader = null;
     }
 
+    //This static class holds the one and only instance of the DataSource class
     private static final class DataSourceHolder {
         private static final DataSource dataSource = new DataSource();
     }
 
+    //Only one instance of the class is constructed and every time this method is called it will
+    //return that instance to the caller (singleton)
     public static DataSource getInstance() {
         return DataSourceHolder.dataSource;
     }
 
+    //This method receives a name of a country and creates and returns an object (instance of the Country class)
+    //This object holds data regarding the COVID-19 pandemic and the impact to this country (deaths, confirmed cases)
     public Country getCountryDataAggregated(String aCountry) throws URISyntaxException, IOException, InterruptedException {
 
+        //A http request is sent to the api,at the endpoint for the country the user requested
+        //and a response is sent back to us
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI("https://covid2019-api.herokuapp.com/v2/country/" + aCountry))
                 .GET()
@@ -45,44 +55,61 @@ public class DataSource {
                 .build()
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-//        System.out.println("1) Status: " + response.statusCode() + ", " + response.body());
-
+        //We use the gson library to create a Java object out of the json response
         Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
 
+        //The object that is created is an instance of the Country class
         Country country =  gson.fromJson(response.body(), Country.class);
+        //A property called type is set to specify that this country object has the aggregated results for a country
+        //If the country's name is null, we did not get any results, so we set the type property accordingly
         if (country.getCountryName() == null) {
             country.setTypeOfData("No Data");
         } else {
             country.setTypeOfData("aggregated");
         }
+        //We return the country object to the caller
         return country;
     }
 
+    //A helper method that reads data from the csv we use to get the data for the countries by month
+    //It returns a list that in each node of the list there is a String array of all the comma separated values
+    //that represents the data of one day for a specific country
+    //THe list have as many nodes as the days of that month
     private List<String[]> getCountryDataForMonthFromCSV(String aCountry, String month, String year) throws IOException {
 
+        //Make the user's input first letter capital and all the other lower case
         String currentCountry = aCountry.substring(0, 1).toUpperCase() + aCountry.substring(1).toLowerCase();
+        //Construct the date as in the csv file
         String date = month + "/" + year;
+        //The list where we add all the data that match the user input
         List<String[]> countryDataForAMonth = new ArrayList<>();
         String line;
         String[] row;
+        //Helper variables to detect when there are no other relevant data, so we stop searching the csv
         boolean doneFindingResults = false;
         boolean atLeastOneResultsFound = false;
 
         try {
+            //load the csv to the BufferedReader
             reader = new BufferedReader(new FileReader(myFile));
 
+            //Read the csv line by line
             while((line = reader.readLine()) != null) {
-
+                //If the current line contains the country and the month we are looking for,
+                //write that line to an array and add that array to the list
                 if(line.contains(date) && line.contains(currentCountry)){
                     atLeastOneResultsFound = true;
                     row = line.split(",");
                     countryDataForAMonth.add(row);
                 } else {
+                    //if we have found a result, and then we found an irrelevant line, this means that
+                    //we will not find any other result because the month we are looking for is finished,
+                    //and we have already written all the data to the list
                     if(atLeastOneResultsFound) {
                         doneFindingResults = true;
                     }
                 }
-
+                //so if this variable is true we stop reading the csv
                 if(doneFindingResults) break;
             }
         } catch (IOException e) {
@@ -90,37 +117,38 @@ public class DataSource {
         } finally {
             reader.close();
         }
+        //return the list of arrays to the caller
         return countryDataForAMonth;
     }
 
+    //This method receives a name of a country and also a month and a year to create and return an object
+    //containing the data for a country for a specific month
     public Country getCountryDataMonthly(String aCountry, String month, String year) throws IOException {
 
         String theMonth = month;
+        //Modify the month to read the proper data
         if (theMonth.length() == 1) theMonth = "0" + theMonth;
+        //Create a list containing the data of each day of the given month from calling the above helper method
         List<String[]> aCountrysDataForAMonth = this.getCountryDataForMonthFromCSV(aCountry, theMonth, year);
 
+        //If the list is empty we create and early return an object with the type set appropriately
         if (aCountrysDataForAMonth.size() == 0) {
             Country noDataForCountry = new Country(null, "", 0);
             noDataForCountry.setTypeOfData("No Data");
             return noDataForCountry;
         }
+        //Now we manually extract the data out of the list we have, and we will create an object out of these data
 
         String location = aCountry;
         int recovered = 0, active = 0;
 
+        //Create 3 lists that we will fill with the data for deaths, confirmed cases, and cumulative
+        //cases of each day of the month
         List<Double> totalCumulativeNumber = new ArrayList<>();
         List<Integer> confirmedList = new ArrayList<>();
         List<Integer> deathsList = new ArrayList<>();
 
-        //Print the lines of the csv that are stored in the list<String[]> aCountrysDataForAMonth
-        //For debugging purposes
-//        for(String[] aCountrysDataForADay: aCountrysDataForAMonth) {
-//            for(String data: aCountrysDataForADay) {
-//                System.out.print(data + ", ");
-//            }
-//            System.out.println();
-//        }
-
+        //we fill these lists with the data
         for(String[] aCountrysDataForADay: aCountrysDataForAMonth) {
             confirmedList.add(Integer.parseInt(aCountrysDataForADay[4]));
             deathsList.add(Integer.parseInt(aCountrysDataForADay[5]));
@@ -128,32 +156,38 @@ public class DataSource {
                 totalCumulativeNumber.add(Double.parseDouble(aCountrysDataForADay[11]));
             }
         }
-
+        //We create a stream to add the data for each day to a sum for the confirmed cases of the whole month
         int confirmed = confirmedList
                 .stream()
                 .reduce(0, Integer::sum);
-
+        //And the same for the deaths
         int deaths = deathsList
                 .stream()
                 .reduce(0, Integer::sum);
-
+        //And a stream to find the mean of the month regarding the cumulative number of active cases for the given month
         double cumulativeNumber = totalCumulativeNumber
                 .stream()
                 .collect(Collectors.averagingDouble(number-> number));
 
+        //We create the data object and fill it with its data
         Data data = new Data(location, confirmed, deaths, recovered, active);
         data.setAverageCumulativeNumberOfCasesFor2Weeks(cumulativeNumber);
         data.setMonth(month);
         data.setYear(year);
 
+        //We create a timestamp
         long queryTimestamp = new Timestamp(System.currentTimeMillis()).getTime();
 
+        //And the date of the search
         SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
         String queryDate = format.format(new Date());
 
+        //We create the country object and fill it with its data,
+        //and we set the type, so we can separate it from the aggregated type,
+        //and know what kind of data is folding
         Country country = new Country(data, queryDate, queryTimestamp);
         country.setTypeOfData("monthly");
-
+        //We return it to the caller
         return country;
     }
 }
